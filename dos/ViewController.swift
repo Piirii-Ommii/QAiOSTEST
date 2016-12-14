7f7e22bb238e0a925567abc5c6c31f7fdfb3d5c3
AleChiquito <ale_vero28@hotmail.com>
2016-12-14 13:18:20 -0600

    p1

--- dos/ViewController.swift
+++ dos/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func btnaction(sender: UIButton) {
+        if(!self.txttexto.text!.isEmpty) {
+            self.lblshow.text = "Hola " + self.txttexto.text!;
+        }
+    }
+    @IBOutlet var lblshow: UILabel!
+    @IBOutlet var txttexto: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
